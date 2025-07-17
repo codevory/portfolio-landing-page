0dc529d8293860c1c2e988a97488332ef25321cc
shahijahan Pedhar <shahijahan.codes@gmail.com>
2025-07-17 23:08:24 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById("themeToggle");
+toggleBtn.addEventListener('click',() => {
+  document.body.classList.toggle('light-color');
+    document.body.classList.toggle('dark-color')
+    toggleBtn.textContent = 
+    document.body.classList.contains("dark-color") ? "🌙" : "🌞";
+    // document.body.classList.contains("dark-color") ? "🌞" : "🌙";
+}
+  )
